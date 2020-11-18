96cfda76e43f704b339be501c8e345273dfca009
Rebecca Haggard <beccahagg5@gmail.com>
2020-11-18 16:23:34 -0700

    Initial Commit

---
+++ CookingThyme/CookingThymeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookingThymeApp.swift
+//  CookingThyme
+//
+//  Created by Rebecca Nybo on 11/18/20.
+//
+
+import SwiftUI
+
+@main
+struct CookingThymeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
